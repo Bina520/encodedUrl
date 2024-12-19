cd71b7874e86deb3fab285c77b3365cb3ef10221
Bina520 <x721527@gmail.com>
2024-12-20 00:29:51 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+ const supabaseUrl = 'https://tioevxcsirewdkvqwqtb.supabase.co';
+ const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpb2V2eGNzaXJld2RrdnF3cXRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ2MjE1MTUsImV4cCI6MjA1MDE5NzUxNX0.h3pE5dCCgZqcizxbLihDyGFD8oBUvnZUaTBWmxvM3M4';
+
+export { supabaseUrl, supabaseAnonKey };
